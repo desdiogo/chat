a7bdedc0f279b3d65f205ae9b757e279e60aa3ed
Diogo Eleutero <desdiogo10@yahoo.com>
2022-07-06 21:54:33 -0300

    fix lint

--- client-vue/src/router/index.ts
+++ client-vue/src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import { routes } from "./routes";
 import { LocalStorage, Route } from "@/enums";
-import {useAuthStore, useTokenStore} from "@/stores";
+import { useAuthStore, useTokenStore } from "@/stores";
 import { useAuth } from "@/composables/useAuth";
 import { LocalStorage as Storage } from "quasar";
 
